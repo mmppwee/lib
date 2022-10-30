759f26db4fbc1e989e6da47083b5d25189e96896
Pornpawee Wareepornthep <pwareepo@z3t1c4.42bangkok.com>
2022-10-30 20:13:42 +0700

    i

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: pwareepo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 18:57:07 by pwareepo          #+#    #+#             */
-/*   Updated: 2022/10/30 19:07:09 by pwareepo         ###   ########.fr       */
+/*   Updated: 2022/10/30 20:06:38 by pwareepo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,27 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-		
+	int	i;
+
+	i = 0;
+	while (s[i] != c)
+		i++;
+	return ((char*)&s[i]);
+}
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+   const char str[] = "http://www.tutorialspoint.com";
+   const char ch = 'a';
+   char *ret;
+
+   ret = ft_strchr(str, ch);
+
+   printf("|%s|\n", ret);
+   
+   return(0);
+}	
